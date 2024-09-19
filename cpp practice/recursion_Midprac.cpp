9cec6674734ecdbf41ce06894f3abc357791aa51
RayyanUrRehman <iamrayyanurrehman2004@gmail.com>
2024-09-19 10:26:51 +0500

    update

---
+++ cpp practice/recursion_Midprac.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// Write a recursive function CountOnes that takes an unsigned integer and return the number of ones 
+// in binary representation of that given number. For an example: if 28 is given as an argument to this 
+// function it will return 3, as 28 is 11100.
+
+int CountOnes(int num)
+{   
+    
+}
+
+
+int main()
+{
+
+}
--- cpp practice/recursion_binarySearch.cpp
+++ cpp practice/recursion_binarySearch.cpp
@@ -1,14 +1,32 @@
 #include<iostream>
 using namespace std;
 
+// Implement a recursive binary search function. Given a sorted array and a target value, 
+// determine if the target exists in the array. If it does, return its index; otherwise, return -1
 
-int binarySearch(int arr[],int n,int target)
+
+int binarySearch(int arr[],int target, int lb, int up)
 {
-
+    if (up<lb){
+        cout<<"Target not found!";
+        return -1;
+    }
+    int mid = (up+lb)/2;
+    if (arr[mid] == target){
+        cout<<"Target found! at index "<<mid;
+        return mid;
+    }
+    else if(mid < target){ 
+        binarySearch(arr,target,mid+1,up);
+    }
+    else{
+        binarySearch(arr,target,lb,mid-1);
+    }
 }
 
 
 int main()
 {
-
+    int arr[8] = {1,2,3,4,5,6,7,8};
+    binarySearch(arr,4,0,7);
 }
