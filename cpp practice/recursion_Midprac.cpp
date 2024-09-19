31502e9f90d219168960815742bcf03a3c1f8b02
RayyanUrRehman <iamrayyanurrehman2004@gmail.com>
2024-09-19 11:17:18 +0500

    update

--- cpp practice/recursion_Midprac.cpp
+++ cpp practice/recursion_Midprac.cpp
@@ -5,13 +5,25 @@ using namespace std;
 // in binary representation of that given number. For an example: if 28 is given as an argument to this 
 // function it will return 3, as 28 is 11100.
 
+
 int CountOnes(int num)
 {   
-    
+     
+    if (num==0) 
+        return 0; 
+    else if (num==1) 
+        return 1; 
+    else 
+    { 
+        return CountOnes(num/2)+ CountOnes(num%2); 
+    } 
+ 
+
+
 }
 
 
 int main()
 {
-
+    cout<<CountOnes(28);
 }
