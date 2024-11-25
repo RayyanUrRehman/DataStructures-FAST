ff5e8a0299fadac7b0765614ef91740daf79b9ae
RayyanUrRehman <iamrayyanurrehman2004@gmail.com>
2024-11-25 19:57:05 +0500

    update

--- cpp practice/hashing.cpp
+++ cpp practice/hashing.cpp
@@ -18,8 +18,11 @@ class hashTable
     Node** arr;
     int size = 10;
 
-    hashTable(){
-        arr = new Node*[size]();
+    hashTable() {
+        arr = new Node*[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = nullptr; 
+        }
     }
 
     int hashFunc(int k)
@@ -31,8 +34,8 @@ class hashTable
     {   
         int hashNum = hashFunc(k);
         Node* newNode = new Node(k,v);
-        if (arr[hashNum]->next == nullptr){
-            arr[hashNum]->next = newNode;
+        if (arr[hashNum] == nullptr){
+            arr[hashNum] = newNode;
         }
         else{
             Node* current = arr[hashNum];
@@ -42,11 +45,38 @@ class hashTable
             current->next = newNode;
         }
     }
+
+    void print()
+    {
+        for (int i=0;i<size;i++){
+            Node* current = arr[i];
+            if (arr[i] != nullptr){
+                cout<<i<<" : ";
+                while(current != nullptr){
+                    cout<<current->val<<" ";
+                    current = current->next;
+                }
+                cout<<endl;
+            }
+        }
+    }
 };
 
 
 
 int main()
 {
+    hashTable table;
+    table.insert(10,20);
+    table.insert(20,30);
+    table.insert(30,40);
+    table.insert(29,30);
+    table.insert(27,90);
+    table.insert(23,60);
+    table.insert(27,50);
+
+    table.print();
+
+
     return 0;
 }
