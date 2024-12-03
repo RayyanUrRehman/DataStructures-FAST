446f1600fe49252413e72169f56b6a01b0084a52
RayyanUrRehman <iamrayyanurrehman2004@gmail.com>
2024-12-03 09:05:47 +0500

    update

--- cpp practice/heap-prac2.cpp
+++ cpp practice/heap-prac2.cpp
@@ -50,7 +50,56 @@ class heap
 
     void del()
     {   
+        if (index == 0){
+            cout<<"Empty heap";
+        }
 
+        index--;
+        arr[0] = arr[index];
+
+        int i = 0;
+        while(i < index){
+            int left = (i * 2) + 1;
+            int right = (i * 2) + 2;
+            int largest = i;
+
+            if (left < index && arr[left] > arr[largest]){
+                largest = left;
+            }
+            if (right < index && arr[right] > arr[largest]){
+                largest = right;
+            }
+
+            if (largest != i){
+                swap(arr[largest], arr[i]);
+                i = largest;
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+    void minHeapify(int i)
+    {
+        while(i < index){
+            int left = (i*2)+1;
+            int right = (i*2)+2;
+            int smallest = i;
+
+            if (left < index && arr[i] > arr[left]){
+                smallest = left;
+            }
+            if (right < index && arr[i] > arr[right]){
+                smallest = right;
+            }
+
+            if (smallest != i){
+                swap(arr[i], arr[smallest]);
+                minHeapify(smallest);
+            }
+            
+        }
     }
 };
 
@@ -58,7 +107,7 @@ class heap
 
 int main()
 {
-    heap h(7);
+    heap h(8);
     h.insert(25);
     h.insert(10);
     h.insert(35);
@@ -66,8 +115,19 @@ int main()
     h.insert(30);
     h.insert(20);
     h.insert(15);
+    h.insert(40);
 
     h.print();
+    cout<<endl;
+
+    // h.del();
+    // h.print();
+
+    // for (int i=(h.index/2-1);i>0;i++){
+    //     h.minHeapify(i);
+    // }
+    h.print();
+
 
     return 0;
 }
